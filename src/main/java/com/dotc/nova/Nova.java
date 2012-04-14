feef59e24b6ee6b52cddc76a546df7de2259db73
Oliver Dotzauer <oliver.dotzauer@gmail.com>
2012-04-14 23:53:49 +0200

    make Filesystem accessible

--- src/main/java/com/dotc/nova/Nova.java
+++ src/main/java/com/dotc/nova/Nova.java
@@ -1,6 +1,7 @@
 package com.dotc.nova;
 
 import com.dotc.nova.events.EventEmitter;
+import com.dotc.nova.filesystem.Filesystem;
 import com.dotc.nova.timers.Timers;
 
 public class Nova {
@@ -9,6 +10,7 @@ public class Nova {
 	private final Timers timers;
 	private final EventEmitter eventEmitter;
 	private final com.dotc.nova.process.Process process;
+	private final Filesystem filesystem;
 
 	public Nova() {
 		processingLoop = new ProcessingLoop();
@@ -17,6 +19,7 @@ public class Nova {
 		timers = new Timers(processingLoop);
 		eventEmitter = new EventEmitter(processingLoop);
 		process = new com.dotc.nova.process.Process(processingLoop);
+		filesystem = new Filesystem(processingLoop);
 	}
 
 	public EventEmitter getEventEmitter() {
@@ -29,6 +32,10 @@ public class Nova {
 
 	public Timers getTimers() {
 		return timers;
+	}
+
+	public Filesystem getFilesystem() {
+		return filesystem;
 	}
 
 }
