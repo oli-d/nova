a088922bb558ff04d2612eab2dcd94c845a78796
Oliver Dotzauer <oliver.dotzauer@gmail.com>
2017-02-04 17:47:07 +0100

    brought back the examples. still not compiling

---
+++ src/test/java/ch/squaredesk/nova/examples/events/mousemirror/TargetFrame.java
@@ -0,0 +1,28 @@
+/*
+ * Copyright (c) Squaredesk GmbH and Oliver Dotzauer.
+ *
+ * This program is distributed under the squaredesk open source license. See the LICENSE file
+ * distributed with this work for additional information regarding copyright ownership. You may also
+ * obtain a copy of the license at
+ *
+ *   https://squaredesk.ch/license/oss/LICENSE
+ */
+
+package ch.squaredesk.nova.examples.events.mousemirror;
+
+import java.awt.Color;
+
+import javax.swing.JFrame;
+
+public class TargetFrame extends JFrame {
+
+	public TargetFrame() {
+		super("Target Frame");
+
+		// some GUI stuff
+		setBackground(Color.CYAN);
+		setDefaultCloseOperation(JFrame.DO_NOTHING_ON_CLOSE);
+		setResizable(false);
+	}
+
+}
---
+++ src/test/java/ch/squaredesk/nova/examples/timers/Clock.java
@@ -0,0 +1,73 @@
+/*
+ * Copyright (c) Squaredesk GmbH and Oliver Dotzauer.
+ *
+ * This program is distributed under the squaredesk open source license. See the LICENSE file
+ * distributed with this work for additional information regarding copyright ownership. You may also
+ * obtain a copy of the license at
+ *
+ *   https://squaredesk.ch/license/oss/LICENSE
+ */
+
+package ch.squaredesk.nova.examples.timers;
+
+import java.awt.BorderLayout;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
+import javax.swing.JFrame;
+import javax.swing.JLabel;
+
+import ch.squaredesk.nova.Nova;
+
+public class Clock extends JFrame {
+	private SimpleDateFormat dateFormatter = new SimpleDateFormat("HH:mm:ss");
+	private JLabel label = new JLabel();
+
+	public Clock() {
+		super("Nova Timers example");
+		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		setSize(200, 100);
+		getContentPane().add(label, BorderLayout.CENTER);
+		setLocationRelativeTo(null);
+	}
+
+	public static void main(String[] args) {
+		Clock clock = new Clock();
+		clock.setVisible(true);
+		clock.tickCurrentTime();
+	}
+
+	private void tickCurrentTime() {
+		/**
+		 * <pre>
+		 * *********************************************************************** * 
+		 * *********************************************************************** * 
+		 * ***                                                                 *** *
+		 * *** 1st step:                                                       *** *
+		 * *** Initilize Nova by creating a new instance of Nova *** *
+		 * ***                                                                 *** *
+		 * *********************************************************************** *
+		 * *********************************************************************** *
+		 */
+		Nova nova = new Nova.Builder().build();
+
+		/**
+		 * <pre>
+		 * *********************************************************************** * 
+		 * *********************************************************************** * 
+		 * ***                                                                 *** *
+		 * *** 2nd step:                                                       *** *
+		 * *** Register a callback, which will be executed periodically        *** *
+		 * ***                                                                 *** *
+		 * *********************************************************************** *
+		 * *********************************************************************** *
+		 */
+		nova.timers.setInterval(new Runnable() {
+
+			@Override
+			public void run() {
+				label.setText(dateFormatter.format(new Date()));
+			}
+		}, 1000); // 1000ms == 1sec
+	}
+}
