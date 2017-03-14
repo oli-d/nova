60a1861b363bcbc3344691f3850e8c85b0c58bb7
Oliver Dotzauer <oliver.dotzauer@gmail.com>
2017-03-14 15:19:54 +0100

    fix config when instantiating EventBus

--- src/main/java/ch/squaredesk/nova/Nova.java
+++ src/main/java/ch/squaredesk/nova/Nova.java
@@ -69,6 +69,8 @@ public class Nova {
                 metrics = new Metrics();
             }
 
+            eventBusConfig = new EventBusConfig(defaultBackpressureStrategy, warnOnUnhandledEvent);
+
             return new Nova(this);
         }
     }
