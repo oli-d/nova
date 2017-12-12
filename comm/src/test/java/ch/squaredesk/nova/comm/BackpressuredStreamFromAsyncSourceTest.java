9a4574d92add38b2030902b7156076ea1127b836
Oliver Dotzauer <oliver.dotzauer@gmail.com>
2017-12-12 18:18:34 +0100

    introduce BackpressuredStreamFromAsyncSource

---
+++ comm/src/test/java/ch/squaredesk/nova/comm/BackpressuredStreamFromAsyncSourceTest.java
@@ -0,0 +1,42 @@
+package ch.squaredesk.nova.comm;
+
+import io.reactivex.schedulers.Schedulers;
+import org.hamcrest.Matchers;
+import org.hamcrest.junit.MatcherAssert;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import java.util.concurrent.CountDownLatch;
+import java.util.concurrent.TimeUnit;
+import java.util.concurrent.atomic.AtomicBoolean;
+
+import static org.hamcrest.Matchers.is;
+import static org.hamcrest.junit.MatcherAssert.assertThat;
+import static org.junit.jupiter.api.Assertions.*;
+
+class BackpressuredStreamFromAsyncSourceTest {
+    @Test
+    void onNextThrowsIfStreamWasAlreadyCompleted() {
+        BackpressuredStreamFromAsyncSource<String> sut = new BackpressuredStreamFromAsyncSource<>();
+        sut.onComplete();
+
+        IllegalStateException ex = assertThrows(
+                IllegalStateException.class,
+                () -> sut.onNext("Hello"));
+        assertThat(ex.getMessage(), is("Stream closed"));
+    }
+
+    @Test
+    void onCompleteInvokesCloseAction() throws Exception {
+        CountDownLatch cdl = new CountDownLatch(1);
+        Runnable closeAction = () -> cdl.countDown();
+        BackpressuredStreamFromAsyncSource<String> sut = new BackpressuredStreamFromAsyncSource<>(closeAction);
+
+        sut.toFlowable().subscribeOn(Schedulers.io()).subscribe();
+        sut.onComplete();
+
+        cdl.await(1, TimeUnit.SECONDS);
+        assertThat(cdl.getCount(), is(0L));
+    }
+
+}
